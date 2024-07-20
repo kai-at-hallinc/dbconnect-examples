6a1a76df89f4ed7592a506c2dcecbd0787a12bd0
kaimhall <kaimhall@gmail.com>
2024-07-20 13:52:07 +0300

    deleted resource i have no time to go through at the moment. working python etl pipe.

--- python/ETL/main.py
+++ python/ETL/main.py
@@ -1,48 +1,43 @@
-from databricks.connect import DatabricksSession
+# Databricks notebook source
 
+# imports
+from databricks.connect import DatabricksSession
 import transformations as trans
 
-def main():
-  # For more detail on configuring the connection properties for your
-  # Databricks workspace please refer to this documentation:
-  # https://docs.databricks.com/en/dev-tools/databricks-connect/python/install.html#configure-connection-python
+# create Spark session:
+spark = DatabricksSession.builder.profile("vscode").getOrCreate()
 
-  # If you have a default profile set in your .databrickscfg no additional code
-  # changes are needed.
-  spark = DatabricksSession.builder.serverless().getOrCreate()
+# Step 1 - Load the Bronze Tables
+print("Step 1: ingesting data into bronze tables")
 
-  # Alternate way to configure your Spark session:
-  # spark = DatabricksSession.builder.profile("PROFILE").clusterId("CLUSTER_ID").getOrCreate()
+table_name_bronze = "main.default.taxi_demo_bronze"
+df = trans.load_data(spark)
+trans.save_data(df, table_name_bronze)
 
-  print(spark.range(100).collect())
+# Step 2 - Load the Silver Tables
+print("Step 2: transforming data into silver tables")
 
-  # Step 1 - Load the Bronze Tables
-  print("Step 1 .....")
-  table_name_bronze = "main.martingrund.taxi_demo_bronze"
-  df = trans.load_data(spark)
-  trans.save_data(df, table_name_bronze)
+df = spark.table(table_name_bronze)
+df = trans.filter_columns(df)
+df = trans.transform_columns(df)
+df = trans.filter_invalid(df)
 
-  # Step 2 - Load the Silver Tables
-  print("Step 2 .....")
-  df = spark.table(table_name_bronze)
-  df = trans.filter_columns(df)
-  df = trans.transform_columns(df)
-  df = trans.filter_invalid(df)
+table_name_silver = "main.default.taxi_demo_silver"
+trans.save_data(df, table_name_silver)
 
-  table_name_silver = "main.martingrund.taxi_demo_silver"
-  trans.save_data(df, table_name_silver)
-
-  print(spark.table(table_name_silver).count())
-
-  # Step 3 Prepare the views
-  print("Step 3 .....")
-  yellow_view = "main.martingrund.taxi_demo_view_yellow"
-  green_view = "main.martingrund.taxi_demo_view_green"
-  trans.create_view(spark, table_name_silver, yellow_view, 1)
-  trans.create_view(spark, table_name_silver, green_view, 2)
-
-if __name__ == "__main__":
-  main()
+print(f"silver row count: {spark.table(table_name_silver).count()}")
 
 
+# Step 3 Prepare the views
+print("Step 3: creating gold layer views")
 
+yellow_view = "main.default.taxi_demo_view_yellow"
+green_view = "main.default.taxi_demo_view_green"
+
+trans.create_view(spark, table_name_silver, yellow_view, 1)
+trans.create_view(spark, table_name_silver, green_view, 2)
+
+
+# query the views
+spark.sql(f"SELECT * FROM {yellow_view}").show(5)
+spark.sql(f"SELECT * FROM {green_view}").show(5)
--- python/ETL/transformations/steps.py
+++ python/ETL/transformations/steps.py
@@ -1,6 +1,13 @@
 from pyspark.sql.functions import col, lit, expr, when
 import pyspark.sql.functions as F
-from pyspark.sql.types import *
+from pyspark.sql.types import (
+    StructField,
+    StructType,
+    StringType,
+    IntegerType,
+    DoubleType,
+    TimestampType,
+)
 from pyspark.sql import SparkSession, DataFrame
 
 
@@ -9,7 +16,14 @@ import time
 
 
 def load_data(spark: SparkSession):
-    """Creates and loads the data from files into a DF"""
+    """
+    Creates a schema and gets yellow and green taxi data from databricks samples. loads the data into a DF. \n
+    params:
+    - spark: spark session \n
+    returns:
+        df: Union dataframe of yellow and green taxi data
+    """
+
     nyc_schema = StructType(
         [
             StructField("vendor", StringType(), True),
@@ -53,6 +67,14 @@ def load_data(spark: SparkSession):
 
 
 def filter_columns(df: DataFrame) -> DataFrame:
+    """
+    filters the columns of the dataframe \n
+    params:
+    - df: dataframe to filter \n
+    returns:
+        columns: vendor, passenger_count, trip_distance, fare_amount, total_amount, tolls_amount
+    """
+
     return df.select(
         df.vendor,
         df.passenger_count,
@@ -64,6 +86,13 @@ def filter_columns(df: DataFrame) -> DataFrame:
 
 
 def transform_columns(df: DataFrame) -> DataFrame:
+    """
+    transform dataframe to have passenger_type, has_tolls and amount_rounded columns. \n
+    params:
+    - df: dataframe to transform \n
+    returns:
+        columns: vendor, passenger_type, has_tolls, distance, amount
+    """
     cleaned = (
         df.withColumn(
             "passenger_type",
@@ -72,6 +101,7 @@ def transform_columns(df: DataFrame) -> DataFrame:
         .withColumn("has_tolls", F.when(df.tolls > 0, True).otherwise(False))
         .withColumn("amount_rounded", F.ceil(df.amount))
     )
+
     cleaned = cleaned.select(
         cleaned.vendor,
         cleaned.passenger_type,
@@ -83,18 +113,49 @@ def transform_columns(df: DataFrame) -> DataFrame:
 
 
 def filter_invalid(df: DataFrame) -> DataFrame:
+    """
+    filters the dataframe to remove zero distance and abnormal distance/amount ratio.\n
+    params:
+    - df: dataframe to filter\n
+    returns: df: filtered dataframe
+    """
     filtered = df.filter(df.distance > 0).filter(df.amount > 0)
     filtered = filtered.filter(~((filtered.distance < 5) & (filtered.amount > 100)))
     return filtered
 
 
 def save_data(df: DataFrame, name: str) -> None:
+    """
+    saves dataframe data as a delta table using overwrite mode. \n
+
+    params:
+    - df: dataframe to save
+        name: name of the table \n
+    returns:
+        none
+    """
     df.write.saveAsTable(name, format="delta", mode="overwrite")
 
 
 def create_view(spark: SparkSession, source: str, dest: str, vendor: int) -> None:
+    """
+    Creates or replaces a view by selecting all columns except 'vendor' from the 'source' table
+    where the 'vendor' column matches the specified 'vendor' value. \n
+    params:
+    - spark: The SparkSession object.
+    - source: The name of the source table.
+    - dest: The name of the destination view.
+    - vendor: The value of the 'vendor' column to filter on. \n
+
+    returns:
+        None
+    """
+
     spark.sql(
         f"""
-  create or replace view {dest} as select * except(vendor) from {source} where vendor = {vendor}
-  """
+        create or replace view {dest} as 
+        select * except(vendor) 
+        from {source} 
+        where vendor = {vendor}
+        """
     )
